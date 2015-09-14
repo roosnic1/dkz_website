7f9d0a15f1a8ff0c21d362b743375e69a307db43
Nicolas Roos <nicolas.roos@serranetga.com>
2015-09-14 11:20:55 +0200

    defined fields for member model

--- app/models/member.js
+++ app/models/member.js
@@ -1,5 +1,10 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  
+  name: DS.attr('String'),
+  func: DS.attr('String'),
+  board: DS.attr('boolean'),
+  description: DS.attr('String'),
+  image: DS.attr('String'),
+  mail: DS.attr('String')
 });
