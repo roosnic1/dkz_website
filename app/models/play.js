ef45df4f9556efec8675d922c3df2d993be8c311
Nicolas Roos <nicolas.roos@serranetga.com>
2015-09-14 11:10:13 +0200

    defined fields for play model

--- app/models/play.js
+++ app/models/play.js
@@ -1,5 +1,26 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  
+    title: DS.attr('String'),
+    subtitle: DS.attr('String'),
+    description: DS.attr('String'),
+    year: DS.attr('Date'),
+    location: DS.attr('String'),
+    picture: DS.attr('String'),
+    //Playdates
+    director: DS.attr('String'),
+    writer: DS.attr('String'),
+    orgWriter: DS.attr('String'),
+    music: DS.attr('String'),
+    actors: DS.attr('String'),
+    assistant: DS.attr('String'),
+    costume: DS.attr('String'),
+    makeup: DS.attr('String'),
+    builders: DS.attr('String'),
+    tech: DS.attr('String'),
+    design: DS.attr('String'),
+    helpers: DS.attr('String'),
+    sales: DS.attr('Sring'),
+    eveningSales: DS.attr('String'),
+    pdfLink: DS.attr('String')
 });
