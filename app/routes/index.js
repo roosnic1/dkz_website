c09591f8d0ec429258a430e54aa89dfc8fabdee2
Nicolas Roos <nicolas.k.roos@gmail.com>
2015-09-27 15:40:53 +0200

    improbed index route and controller to display newest Post

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+	model: function() {
+		return Ember.RSVP.hash({
+			plays: this.store.findAll('play'),
+			members: this.store.findAll('member'),
+			posts: this.store.findAll('post')
+		});
+	}
 });
