aa9fb73fc16142ba6a9383fd471d1be1b1e182f9
Nicolas Roos <nicolas.k.roos@gmail.com>
2015-10-10 11:49:13 +0200

    renmaned title property to name

--- app/models/post.js
+++ app/models/post.js
@@ -1,7 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  title: DS.attr('String'),
+  name: DS.attr('String'),
   text: DS.attr('String'),
   author: DS.attr('String'),
   created: DS.attr('Number'),
