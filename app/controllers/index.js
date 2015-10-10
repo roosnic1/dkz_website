492f571c0b296b2836d46c507fc41b3e0d598ad7
Nicolas Roos <nicolas.k.roos@gmail.com>
2015-10-10 21:18:11 +0200

    load play-preview in index and added styles

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -2,8 +2,11 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 	newestPost: Ember.computed('model.posts.[]',function() {
-		console.log('test');
 		return this.get('model.posts').sortBy('created').get('lastObject');
+	}),
+
+	newestPlay: Ember.computed('model.plays.[]', function() {
+		return this.get('model.plays').sortBy('created').get('lastObject');
 	})
 
 });
