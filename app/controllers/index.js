2afdba38773ba23319891601dcc74d2281db4346
Nicolas Roos <nicolas.k.roos@gmail.com>
2015-10-12 20:54:42 +0200

    implemented play preview gallery

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -5,8 +5,23 @@ export default Ember.Controller.extend({
 		return this.get('model.posts').sortBy('created').get('lastObject');
 	}),
 
-	newestPlay: Ember.computed('model.plays.[]', function() {
-		return this.get('model.plays').sortBy('created').get('lastObject');
-	})
+	playIndex: 0,
+
+	play: Ember.computed('model.plays.[]', 'playIndex', function() {
+		//Use reverse() to get the youngest play first.
+		return this.get('model.plays').sortBy('year').reverse()[this.get('playIndex')];
+	}),
+
+	actions: {
+		nextPlay: function(back) {
+			var pi = this.get('playIndex');
+			if(!back && pi++ === this.get('model.plays.length')) {
+				pi = 0;
+			} else if(back && pi-- === 0) {
+				pi = this.get('model.plays.length') - 1;
+			}
+			this.set('playIndex',pi);
+		}
+	}
 
 });
