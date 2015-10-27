9f20811bdea681921acac6c3c456d78755189501
Nicolas Roos <nicolas.k.roos@gmail.com>
2015-10-27 22:14:09 +0100

    create dir if it doesn't exist

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,7 @@
 set -ev
 if [ "${TRAVIS_BRANCH}" = "master" ]; then
 	echo "Master Branch"
+	ssh -i deploy deploy@vsg-xebaci.cyoncloud.com 'mkdir -p /var/www/html/diekulissezug.ch/client'
 	scp -i deploy -r dist/* deploy@vsg-xebaci.cyoncloud.com:/var/www/html/diekulissezug.ch/client
 fi
 
