44735ede48e1ad7f520dfb1dccd2c0d93329db63
Nicolas Roos <nicolas.k.roos@gmail.com>
2015-10-28 15:00:16 +0100

    added goToPlay method

--- app/routes/application.js
+++ app/routes/application.js
@@ -6,6 +6,9 @@ export default Ember.Route.extend({
 	actions: {
 		goToPost: function(id) {
 			this.transitionTo('posts.post',id);
+		},
+		goToPlay: function(id) {
+			this.transitionTo('plays.play',id);
 		}
 	}
 });
