cb94c4f23dcf76c881c14b203464c0991a7f1917
Nicolas Roos <nicolas.k.roos@gmail.com>
2015-10-28 16:18:13 +0100

    added sorting for plays overview

--- app/routes/plays/index.js
+++ app/routes/plays/index.js
@@ -3,5 +3,14 @@ import Ember from 'ember';
 export default Ember.Route.extend({
 	model: function() {
 		return this.store.findAll('play');
+	},
+
+	setupController: function(controller,model) {
+		controller.set('model',model);
+		controller.set('sortProperties',['year:desc']);
+		controller.set('sortedModel',Ember.computed.sort('model','sortProperties'));
+
+		//controller.set('model',model.sortBy('year').reverse());
 	}
+
 });
