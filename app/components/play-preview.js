6880e5f5d146492f63f8b63ad3c9cc8e65625bc9
Nicolas Roos <nicolas.k.roos@gmail.com>
2015-10-28 14:59:57 +0100

    added click action to play-preview method

--- app/components/play-preview.js
+++ app/components/play-preview.js
@@ -1,5 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-	tagName: 'playpre'
+	tagName: 'playpre',
+
+	click: function(event) {
+		this.sendAction('action',this.get('model.id'));
+	}
 });
